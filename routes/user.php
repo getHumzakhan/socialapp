994e4e8f89b50f6bde75802764624e8a923a0cbb
Hamza Khan <gethumzakhan@gmail.com>
2021-11-22 16:46:33 +0500

    renamed signup route to user

--- routes/user.php
+++ routes/user.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\SignupController;
 
 Route::post('/signup', [SignupController::class, 'signup'])->middleware('signup');
