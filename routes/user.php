af950d8f06dd9d9419350e5ee45ff72ab0e20344
Hamza Khan <gethumzakhan@gmail.com>
2021-11-24 02:51:35 +0500

    sign in route added

--- routes/user.php
+++ routes/user.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User;
 
 Route::post('/signup', [User::class, 'signup']);
-
 Route::get('/verifyAccount/{token}', [User::class, 'verify_signup_token']);
+
+Route::post('/sigin', [User::class, 'signin']);
