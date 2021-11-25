aabe5e7de34f6ec3756291dcdf25a03d036fad3d
Hamza Khan <gethumzakhan@gmail.com>
2021-11-25 17:13:21 +0500

    separated mongoDB instance creation

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers;
 use \App\Http\Requests\SignupRequest;
 use \App\Notifications\SignupNotification as Notification;
 use \App\Services\Response\Api;
-use MongoDB;
+use MongoDB\Client as MongoDB;
 
 class User extends Controller
 {
+    protected $db;
+
+    public function __construct()
+    {
+        $database = new MongoDB();
+        $this->db = $database->socialapp;
+    }
 
     public function signup(SignupRequest $request_data)
     {
-        //Register User
-        $collection = (new MongoDB\Client)->socialapp->users;
         $document = $request_data->all();
-        $insertOneResult = $collection->insertOne($document);
+        //Register User
+        $this->db->users->insertOne($document);
 
         //send email for account verification
         $notification = $request_data->all();
@@ -27,8 +33,7 @@ class User extends Controller
 
     public function verify_signup_token($token)
     {
-        $collection = (new MongoDB\Client)->socialapp->users;
-        $document = $collection->findOne(["verificationToken" => $token]);
+        $this->db->users->findOne(["verificationToken" => $token]);
 
         if (isset($document['verificationToken'])) {
 
@@ -42,7 +47,6 @@ class User extends Controller
     //set isVerified flag to true in DB upon account confirmation
     public function set_verified_flag($user_id)
     {
-        $collection = (new MongoDB\Client)->socialapp->users;
-        $collection->updateOne(['_id' => $user_id], ['$set' => ['isVerified' => true]]);
+        $this->db->users->updateOne(['_id' => $user_id], ['$set' => ['isVerified' => true]]);
     }
 }
