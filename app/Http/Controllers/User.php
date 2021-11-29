c7d91a5244831c98430d2153195cd7a300e4db45
Hamza Khan <gethumzakhan@gmail.com>
2021-11-30 04:12:38 +0500

    aaa

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -74,18 +74,18 @@ class User extends Controller
 
     //params: merged array having user id as _id and request data
     //uploades file and stores its url in db
-    public function create_post(CreatePostRequest $request_data)
-    {
-        $user_id = strval($request_data['_id']);   //user id who is creating post
-        $text = $request_data['text'];
-        $file = $request_data->file('attachment');
+    // public function create_post(CreatePostRequest $request_data)
+    // {
+    //     $user_id = strval($request_data['_id']);   //user id who is creating post
+    //     $text = $request_data['text'];
+    //     $file = $request_data->file('attachment');
 
-        $destinationPath = 'uploads/' . strval($request_data['_id']);
-        $file->move($destinationPath, $file->getClientOriginalName());
-        $url = $destinationPath . "/" . $file->getClientOriginalName();
+    //     $destinationPath = 'uploads/' . strval($request_data['_id']);
+    //     $file->move($destinationPath, $file->getClientOriginalName());
+    //     $url = $destinationPath . "/" . $file->getClientOriginalName();
 
-        $post = array("user_id" => $user_id, "text" => $text, "attachment" => base64_encode($url), "created_at" => date("d-m-y h-i-sa"));
-        $this->db->posts->insertOne($post);
-        return API::response(["Message" => "Post Successfully Created"], 200);
-    }
+    //     $post = array("user_id" => $user_id, "text" => $text, "attachment" => base64_encode($url), "created_at" => date("d-m-y h-i-sa"));
+    //     $this->db->posts->insertOne($post);
+    //     return API::response(["Message" => "Post Successfully Created"], 200);
+    // }
 }
